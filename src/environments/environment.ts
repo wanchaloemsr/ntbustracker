3fdb31053f7700540c1129bd84f2b740e201a606
Wanchaloem Sriwiset <golf.sriwise@hotmail.com>
2017-08-15 07:06:09 +0930

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,10 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  mapbox: {
+  	accessToken: 'pk.eyJ1IjoiZ29sZjIzMDUwIiwiYSI6ImNqNmNuZnpuczFwangzMnFyM2hseTlvNHIifQ.ZOZBGzuYqqc-mqEAJ6ZX0A'
+
+  }
 };
