d90c1261fa204135d5f6abb781ba071ec2e338e5
Wanchaloem Sriwiset <golf.sriwise@hotmail.com>
2017-09-12 23:26:49 +0930

    Minor Interface changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { FormsModule } from '@angular/forms'
 import { HttpModule } from '@angular/http'
 import { RouterModule, Routes } from '@angular/router';
 
-import { DevExtremeModule } from 'devextreme-angular';
-
 import { AgmCoreModule } from '@agm/core';
 import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
 
@@ -53,8 +51,7 @@ const appRoutes: Routes = [
       apiKey: 'AIzaSyBfiDo0zbigHQFKKir0U6SkBT238M0m3V8'
     }),
     AgmSnazzyInfoWindowModule,
-    NgbModule.forRoot(),
-    DevExtremeModule
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/timetables/route.component.ts
+++ src/app/timetables/route.component.ts
@@ -125,8 +125,6 @@ export class RouteComponent implements OnInit{
 
       }
 
-
-
     }
 
   }
